f8d770e9278887438272d7170caeb329cc5fef5d
Priyam-Pramanik <86661996+Priyam-Pramanik@users.noreply.github.com>
2022-11-05 13:07:03 +0100

    Add files via upload
    init changes

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ var block_image_object= "";
 function new_image(get_image)
 {
 	// to upload images
-	fabric.Image.fromUrl(get_image, function(Img){
+	fabric.Image.fromURL(get_image, function(Img){
 		block_image_object = Img;
 			
 		block_image_object.scaleToHeight(block_image_height);
